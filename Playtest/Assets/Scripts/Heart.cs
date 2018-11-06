801570e9726ea94a4c1063b79112991fd906a589
christt105 <36189169+christt105@users.noreply.github.com>
2018-11-06 13:50:12 +0100

    Heath animation

---
+++ Playtest/Assets/Scripts/Heart.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heart : MonoBehaviour
+{
+
+    private float scale = 1.0F;
+    private bool inc = true;
+    // Use this for initialization
+    void Start()
+    {
+        scale = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inc)
+        {
+            scale += 0.05f;
+        }
+        else
+        {
+            scale -= 0.05f;
+        }
+        if (scale >= 3.0f || scale <= 1.0f)
+        {
+            inc = !inc;
+        }
+
+        gameObject.transform.localScale = new Vector3(scale, scale, 1);
+    }
+}
