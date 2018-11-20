189f7a9323464d08dca791ca39cc6f336fb99aed
christt105 <36189169+christt105@users.noreply.github.com>
2018-11-20 13:27:12 +0100

    Created basic blink script

---
+++ Playtest/Assets/Scripts/Blink.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blink : MonoBehaviour {
+
+    SpriteRenderer white;
+    bool test;
+    Color tmp;
+    // Use this for initialization
+	void Start () {
+        white = GetComponentInChildren<SpriteRenderer>();
+        test = false;
+        tmp = white.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            test = !test;
+        }
+
+        if (test)
+        {
+            white.color = tmp;
+            tmp.a = 0.75f;
+        }
+        else
+        {
+            white.color = tmp;
+            tmp.a = 0.0f;
+        }
+	}
+}
