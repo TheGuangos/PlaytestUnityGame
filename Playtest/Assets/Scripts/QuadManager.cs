fd05a580367115ca8b91881c468374af8a830ef6
christt105 <36189169+christt105@users.noreply.github.com>
2018-11-27 12:42:14 +0100

    Activate quads with arrows

--- Playtest/Assets/Scripts/QuadManager.cs
+++ Playtest/Assets/Scripts/QuadManager.cs
@@ -9,20 +9,49 @@ public class QuadManager : MonoBehaviour {
     public GameObject yellow;
     public GameObject green;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private SpriteRenderer blue_s;
+    private SpriteRenderer red_s;
+    private SpriteRenderer yellow_s;
+    private SpriteRenderer green_s;
+
+    // Use this for initialization
+    void Start () {
+        red_s = red.GetComponentInChildren<SpriteRenderer>();
+        blue_s = blue.GetComponentInChildren<SpriteRenderer>();
+        green_s = green.GetComponentInChildren<SpriteRenderer>();
+        yellow_s = yellow.GetComponentInChildren<SpriteRenderer>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            red.GetComponentInChildren<SpriteRenderer>().enabled = true;
+            if (red_s.enabled)
+                red_s.enabled = false;
+            else
+                red_s.enabled = true;
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            red.GetComponentInChildren<SpriteRenderer>().enabled = false;
+            if (yellow_s.enabled)
+                yellow_s.enabled = false;
+            else
+                yellow_s.enabled = true;
         }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (green_s.enabled)
+                green_s.enabled = false;
+            else
+                green_s.enabled = true;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (blue_s.enabled)
+                blue_s.enabled = false;
+            else
+                blue_s.enabled = true;
+        }
+
     }
 }
