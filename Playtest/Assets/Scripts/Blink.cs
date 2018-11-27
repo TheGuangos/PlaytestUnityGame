d798ac89c7c7bb30debc0cab1df45890c34573b4
christt105 <36189169+christt105@users.noreply.github.com>
2018-11-27 12:28:12 +0100

    Blink red quad with up and down arrow

--- Playtest/Assets/Scripts/Blink.cs
+++ Playtest/Assets/Scripts/Blink.cs
@@ -9,7 +9,7 @@ public class Blink : MonoBehaviour {
     Color tmp;
     // Use this for initialization
 	void Start () {
-        white = GetComponentInChildren<SpriteRenderer>();
+        white = GetComponent<SpriteRenderer>();
         test = false;
         tmp = white.color;
 	}
--- Playtest/Assets/Scripts/QuadManager.cs
+++ Playtest/Assets/Scripts/QuadManager.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class QuadManager : MonoBehaviour {
 
+    public GameObject blue;
+    public GameObject red;
+    public GameObject yellow;
+    public GameObject green;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +16,13 @@ public class QuadManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            red.GetComponentInChildren<SpriteRenderer>().enabled = true;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            red.GetComponentInChildren<SpriteRenderer>().enabled = false;
+        }
+    }
 }
