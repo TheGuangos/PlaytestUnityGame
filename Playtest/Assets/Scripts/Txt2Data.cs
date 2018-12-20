979a3fec29e1a3fc216c1bb7fd4be4208b15de6a
Marc Gálvez <36281630+optus23@users.noreply.github.com>
2018-12-20 02:22:33 +0100

    Create txt when player inits application and LOGIN Date

---
+++ Playtest/Assets/Scripts/Txt2Data.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Txt2Data : MonoBehaviour {
+
+
+    void CreateText()
+    {
+        //PAth of the file
+        string path = Application.dataPath + "/ParseVars.txt";
+        //Create File 
+        if(!File.Exists(path))
+        {
+            File.WriteAllText(path, "PlayTesting - Variables Parsing \n\n");
+        }
+        //Content of the file
+        string login = "Login date: " + System.DateTime.Now + "\n";
+        string counter_click = "Counter value: " + /*TO DO: Acceder al script Quad.cs +*/ "\n";
+        //Add some to text to it
+        File.AppendAllText(path, login);
+        File.AppendAllText(path, counter_click);
+    }
+	// Use this for initialization
+	void Start () {
+        CreateText();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
