45f857ff724aa6f7a599db0bd39199da58fd0e5f
Marc Gálvez <36281630+optus23@users.noreply.github.com>
2018-12-24 01:01:24 +0100

    Declared Game vars

--- Playtest/Assets/Scripts/Txt2Data.cs
+++ Playtest/Assets/Scripts/Txt2Data.cs
@@ -5,30 +5,40 @@ using System.IO;
 
 public class Txt2Data : MonoBehaviour {
 
+    string login;
+    string score;
+    string high_score;
+    string total_time_playing;
+    string time_playing_game;
+    string time_waiting;
+    string number_games;
+    string life_losed;
+
+    // Use this for initialization
+    void Start () {
+        CreateText();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        QuadManager quad_manager = GetComponent<QuadManager>();
+        
+	}
 
     void CreateText()
     {
         //PAth of the file
         string path = Application.dataPath + "/ParseVars.txt";
         //Create File 
-        if(!File.Exists(path))
+        if (!File.Exists(path))
         {
             File.WriteAllText(path, "PlayTesting - Variables Parsing \n\n");
         }
+
         //Content of the file
-        string login = "Login date: " + System.DateTime.Now + "\n";
-        string counter_click = "Counter value: " + /*TO DO: Acceder al script Quad.cs +*/ "\n";
+        login = "Login date: " + System.DateTime.Now + "\n";
         //Add some to text to it
         File.AppendAllText(path, login);
         File.AppendAllText(path, counter_click);
     }
-	// Use this for initialization
-	void Start () {
-        CreateText();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
