3556ec9e322d26e86dd0a0768f596f6edb039f8b
Marc Gálvez <36281630+optus23@users.noreply.github.com>
2018-12-24 01:45:06 +0100

    Logout and time_playing

--- Playtest/Assets/Scripts/Txt2Data.cs
+++ Playtest/Assets/Scripts/Txt2Data.cs
@@ -6,6 +6,7 @@ using System.IO;
 public class Txt2Data : MonoBehaviour {
 
     string login;
+    string logout;
     string score;
     string high_score;
     string total_time_playing;
@@ -16,18 +17,27 @@ public class Txt2Data : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+
         CreateText();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        //PAth of the file
         QuadManager quad_manager = GetComponent<QuadManager>();
-        
 	}
+
+    private void OnApplicationQuit()
+    {
+        string path = Application.dataPath + "/ParseVars.txt";
+        total_time_playing = "Total time playing:  " + Time.realtimeSinceStartup.ToString("F2") + "\n";
+        logout = "----Logout date:  " + System.DateTime.Now + "----\n";
+        File.AppendAllText(path, total_time_playing);
+        File.AppendAllText(path, logout);
+    }
 
     void CreateText()
     {
-        //PAth of the file
         string path = Application.dataPath + "/ParseVars.txt";
         //Create File 
         if (!File.Exists(path))
@@ -36,7 +46,8 @@ public class Txt2Data : MonoBehaviour {
         }
 
         //Content of the file
-        login = "Login date: " + System.DateTime.Now + "\n";
+        login = "----Login date:  " + System.DateTime.Now + "----\n";
+
         //Add some to text to it
         File.AppendAllText(path, login);
     }
